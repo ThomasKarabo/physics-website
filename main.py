6cd7f207b94727abf7a3ec5af02f4b8b828e658d
Karabo Mohlapo <79021656+ThomasKarabo@users.noreply.github.com>
2023-04-11 16:39:45 +0000

    Added a table and a few modifications

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ main = Flask(__name__)
 
 @main.route('/')
 def hello_world():
-  return render_template("physics.html")
+  return render_template("physics.html",company_name='Thomas Inc')
   
 if __name__ == "__main__":
   main.run(host='0.0.0.0', debug=True)
